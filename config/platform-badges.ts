8e0839c44856c2ff5295329c5b57a50dd2de21fd
babluyadav5958 <babluyadav5958@gmail.com>
2026-07-03 00:30:15 +0530

    design changes in promotion page

--- config/platform-badges.ts
+++ config/platform-badges.ts
@@ -10,28 +10,28 @@ export const PLATFORM_BADGES: Badge[] = [
     {
         id: 'spotify',
         name: 'Spotify',
-        logoUrl: 'https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png',
+        logoUrl: '/assets/images/social-platform-logo/spotify-circle-logo-promotion.png',
         fallbackText: 'SP',
         color: '#1DB954'
     },
     {
         id: 'apple-music',
         name: 'Apple Music',
-        logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Apple_Music_logo.svg/2560px-Apple_Music_logo.svg.png',
+        logoUrl: '/assets/images/social-platform-logo/apple-music-circle-icon-promotion.png',
         fallbackText: 'AM',
         color: '#FC3C44'
     },
     {
         id: 'youtube-music',
         name: 'YouTube Music',
-        logoUrl: 'https://music.youtube.com/img/favicon_144.png',
+        logoUrl: '/assets/images/social-platform-logo/youtube-music-circle-logo-promotion.png',
         fallbackText: 'YT',
         color: '#FF0000'
     },
     {
         id: 'amazon-music',
         name: 'Amazon Music',
-        logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Amazon_Music_logo.svg/2560px-Amazon_Music_logo.svg.png',
+        logoUrl: '/assets/images/social-platform-logo/amazon-music-circle-logo-promotion.png',
         fallbackText: 'AZ',
         color: '#00A8E1'
     },
@@ -64,9 +64,16 @@ export const PLATFORM_BADGES: Badge[] = [
         color: '#2C99C9'
     },
     {
+        id: 'instagram',
+        name: 'Instagram',
+        logoUrl: '/assets/images/social-platform-logo/instagram-circle-logo-promotion.png',
+        fallbackText: 'IG',
+        color: '#E1306C'
+    },
+    {
         id: "wynk",
         name: "Wynk Music",
-        logoUrl: "https://wynk.in/assets/images/favicon.ico",
+        logoUrl: '/assets/images/social-platform-logo/wynk-music-circle-logo-promotion.png',
         fallbackText: 'WK',
         color: '#E11B22'
     }
